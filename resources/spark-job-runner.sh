7ebea2a2705f2ca6f0e3a08f1ca3e95e8060a93e
Iqbal Singh <iqbalkhattra85@gmail.com>
2017-09-03 19:07:49 -0600

    Initial commit for Hadoop Spark

---
+++ resources/spark-job-runner.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+##############################################################################################
+#   Script Name : spark-job-runner.sh
+#   Description : Script will pick up the configuration file and will run the spark example
+#   Author      : Iqbal Singh
+#   Version     : 0.1
+##############################################################################################
+
+#Intialize the parameters
+
